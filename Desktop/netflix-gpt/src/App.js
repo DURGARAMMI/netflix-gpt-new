0de9ea86b7bf02821bd578d8cdd8525de57a5281
rameshd <rameshd@turito.com>
2024-02-29 18:39:35 +0530

    initial setup

---
+++ Desktop/netflix-gpt/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-center p-3 shadow-md text-red-700 h-[80px] flex justify-center items-center">
+      Building NETFLIX site! Please check it
+    </div>
+  );
+}
+
+export default App;
